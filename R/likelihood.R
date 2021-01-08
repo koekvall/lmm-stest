ac00dab4d76e842689e164428a2efa243285a3d3
koekvall <k.o.ekvall@gmail.com>
2021-01-08 07:24:45 +0100

    Add warning

--- R/likelihood.R
+++ R/likelihood.R
@@ -6,12 +6,15 @@ loglik <- function(y, X, Z, Beta, sigma, lambda, id, diffs)
   p <- ncol(X)
   stopifnot(is.matrix(Z), nrow(Z) == n)
   q <- ncol(Z)
-  stopifnot(is.atomic(Beta), length(Beta) == p)
-  stopifnot(is.atomic(sigma), length(sigma) == 1, sigma > 0)
-  stopifnot(is.atomic(lambda))
+  stopifnot(is.atomic(Beta), is.null(dim(Beta)), length(Beta) == p)
+  stopifnot(is.atomic(sigma), length(sigma) == 1)
+  if(abs(sigma) < 1e-12){
+    warning("sigma is approximately zero, computations may be unstable")
+  }
+  stopifnot(is.atomic(lambda), is.null(dim(lambda)))
   d <- length(lambda)
-  stopifnot(is.atomic(id), length(id) == q, sort(unique(id)) == 1:d)
+  stopifnot(is.atomic(id), length(id) == q, all(sort(unique(id)) == 1:d))
   stopifnot(diffs %in% 0:2)
-  
+
   loglik_rcpp(y, X, Z, Beta, sigma, lambda, id, diffs)
 }
